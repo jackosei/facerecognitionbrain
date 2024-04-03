c074f69e02d99781e78c836e2faba7e0a9cb7a1c
Jack Osei <jackoseik@gmail.com>
2024-04-03 21:28:38 +0400

    add sign in component and routes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Logo from "../Logo/Logo"
 
-const Navigation = () => {
+const Navigation = ({ onRouteChange }) => {
 	return (
 		<div style={{ display: 'flex', justifyContent: 'space-between' }}>
 			<Logo />
 			<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<p className="f3 link dim white underline  pa4 pt3 pb3 pointer">Sign Out</p>
+				<p
+					className="f3 link dim white underline  pa4 pt3 pb3 pointer"
+					onClick={() => onRouteChange('signin')}
+				>Sign Out</p>
 			</nav>
 		</div >
 	)
